0eb397f102d036e1f459b66f79a0d09a1f160667
Utku Aydın <utkuaydin34@gmail.com>
2015-11-10 12:30:23 +0200

    Update advanced citations when their original citations' raw data changes

--- EventListener/CitationEventSubscriber.php
+++ EventListener/CitationEventSubscriber.php
@@ -15,6 +15,7 @@ class CitationEventSubscriber implements EventSubscriber
     {
         return array(
             Events::prePersist,
+            Events::postUpdate,
         );
     }
 
@@ -24,29 +25,53 @@ class CitationEventSubscriber implements EventSubscriber
         $entityManager = $args->getObjectManager();
 
         if ($entity instanceof Citation) {
-            $raw = $entity->getRaw();
-
-            $client = new Client(['base_uri' => 'http://parser.dergipark.gov.tr']);
-            $response = $client->request('GET', '/', ['query' => ['q' => $raw]]);
-
-            if ($response->getStatusCode() == 200) {
-                $json = $response->getBody()->getContents();
-                $parsedCitation = json_decode($json, true)[0][1];
-
-                $advancedCitation = new AdvancedCitation();
-                $advancedCitation
-                    ->setCitation($entity)
-                    ->setAuthor(!empty($parsedCitation['author']) ? $parsedCitation['author'] : null)
-                    ->setTitle(!empty($parsedCitation['title']) ? $parsedCitation['title'] : null)
-                    ->setEditor(!empty($parsedCitation['editor']) ? $parsedCitation['editor'] : null)
-                    ->setPages(!empty($parsedCitation['pages']) ? $parsedCitation['pages'] : null)
-                    ->setPublisher(!empty($parsedCitation['publisher']) ? $parsedCitation['publisher'] : null)
-                    ->setLocation(!empty($parsedCitation['location']) ? $parsedCitation['location'] : null)
-                    ->setType(!empty($parsedCitation['type']) ? $parsedCitation['type'] : null)
-                    ->setLanguage(!empty($parsedCitation['language']) ? $parsedCitation['language'] : null);
-
-                $entityManager->persist($advancedCitation);
-            }
+            $advancedCitation = $this->prepareAdvancedCitation($entity);
+            $entityManager->persist($advancedCitation);
         }
+    }
+
+    public function postUpdate(LifecycleEventArgs $args)
+    {
+        $entity = $args->getObject();
+        $entityManager = $args->getObjectManager();
+
+        if ($entity instanceof Citation) {
+            $advancedCitation = $entityManager
+                ->getRepository('AdvancedCitationBundle:AdvancedCitation')
+                ->findOneBy(['citation' => $entity]);
+            $advancedCitation = $this->prepareAdvancedCitation($entity, $advancedCitation);
+            $entityManager->persist($advancedCitation);
+            $entityManager->flush();
+        }
+    }
+
+    private function prepareAdvancedCitation(Citation $entity, AdvancedCitation $advancedCitation = null)
+    {
+        $client = new Client(['base_uri' => 'http://parser.dergipark.gov.tr']);
+        $response = $client->request('GET', '/', ['query' => ['q' => $entity->getRaw()]]);
+
+        if ($response->getStatusCode() == 200) {
+            $json = $response->getBody()->getContents();
+            $parsedCitation = json_decode($json, true)[0][1];
+
+            if ($advancedCitation === null) {
+                $advancedCitation = new AdvancedCitation();
+            }
+
+            $advancedCitation
+                ->setCitation($entity)
+                ->setAuthor(!empty($parsedCitation['author']) ? $parsedCitation['author'] : null)
+                ->setTitle(!empty($parsedCitation['title']) ? $parsedCitation['title'] : null)
+                ->setEditor(!empty($parsedCitation['editor']) ? $parsedCitation['editor'] : null)
+                ->setPages(!empty($parsedCitation['pages']) ? $parsedCitation['pages'] : null)
+                ->setPublisher(!empty($parsedCitation['publisher']) ? $parsedCitation['publisher'] : null)
+                ->setLocation(!empty($parsedCitation['location']) ? $parsedCitation['location'] : null)
+                ->setType(!empty($parsedCitation['type']) ? $parsedCitation['type'] : null)
+                ->setLanguage(!empty($parsedCitation['language']) ? $parsedCitation['language'] : null);
+
+            return $advancedCitation;
+        }
+
+        return null;
     }
 }
