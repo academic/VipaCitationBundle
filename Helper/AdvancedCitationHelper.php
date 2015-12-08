2a1a81e7d23f14254a08cdc2714959c8faf7858c
Behram ÇELEN <behramcelen@gmail.com>
2015-12-08 14:52:54 +0200

    extend helper for each field

--- Helper/AdvancedCitationHelper.php
+++ Helper/AdvancedCitationHelper.php
@@ -78,7 +78,27 @@ class AdvancedCitationHelper
                     ->setPublisher(!empty($parsedCitation['publisher']) ? AdvancedCitationHelper::handleField($parsedCitation['publisher']) : null)
                     ->setLocation(!empty($parsedCitation['location']) ? AdvancedCitationHelper::handleField($parsedCitation['location']) : null)
                     ->setType(!empty($parsedCitation['type']) ? AdvancedCitationHelper::handleField($parsedCitation['type']) : null)
-                    ->setLanguage(!empty($parsedCitation['language']) ? AdvancedCitationHelper::handleField($parsedCitation['language']) : null);
+                    ->setAddress(!empty($parsedCitation['adress']) ? AdvancedCitationHelper::handleField($parsedCitation['address']) : null)
+                    ->setAnnote(!empty($parsedCitation['annote']) ? AdvancedCitationHelper::handleField($parsedCitation['annote']) : null)
+                    ->setBooktitle(!empty($parsedCitation['booktitle']) ? AdvancedCitationHelper::handleField($parsedCitation['booktitle']) : null)
+                    ->setChapter(!empty($parsedCitation['chapter']) ? AdvancedCitationHelper::handleField($parsedCitation['chapter']) : null)
+                    ->setCrossref(!empty($parsedCitation['crossref']) ? AdvancedCitationHelper::handleField($parsedCitation['crossref']) : null)
+                    ->setEdition(!empty($parsedCitation['edition']) ? AdvancedCitationHelper::handleField($parsedCitation['edition']) : null)
+                    ->setEprint(!empty($parsedCitation['eprint']) ? AdvancedCitationHelper::handleField($parsedCitation['eprint']) : null)
+                    ->setHowpublished(!empty($parsedCitation['howpublished']) ? AdvancedCitationHelper::handleField($parsedCitation['howpublished']) : null)
+                    ->setJournal(!empty($parsedCitation['journal']) ? AdvancedCitationHelper::handleField($parsedCitation['journal']) : null)
+                    ->setKey(!empty($parsedCitation['key']) ? AdvancedCitationHelper::handleField($parsedCitation['key']) : null)
+                    ->setMonth(!empty($parsedCitation['month']) ? AdvancedCitationHelper::handleField($parsedCitation['month']) : null)
+                    ->setNote(!empty($parsedCitation['note']) ? AdvancedCitationHelper::handleField($parsedCitation['note']) : null)
+                    ->setNumber(!empty($parsedCitation['number']) ? AdvancedCitationHelper::handleField($parsedCitation['number']) : null)
+                    ->setOrganization(!empty($parsedCitation['organization']) ? AdvancedCitationHelper::handleField($parsedCitation['organization']) : null)
+                    ->setPages(!empty($parsedCitation['pages']) ? AdvancedCitationHelper::handleField($parsedCitation['pages']) : null)
+                    ->setSchool(!empty($parsedCitation['school']) ? AdvancedCitationHelper::handleField($parsedCitation['school']) : null)
+                    ->setSeries(!empty($parsedCitation['series']) ? AdvancedCitationHelper::handleField($parsedCitation['series']) : null)
+                    ->setUrl(!empty($parsedCitation['url']) ? AdvancedCitationHelper::handleField($parsedCitation['url']) : null)
+                    ->setVolume(!empty($parsedCitation['volume']) ? AdvancedCitationHelper::handleField($parsedCitation['volume']) : null)
+                    ->setYear(!empty($parsedCitation['year']) ? AdvancedCitationHelper::handleField($parsedCitation['year']) : null)
+                ;
 
                 $entityManager->persist($citation);
                 $entityManager->persist($advancedCitation);
