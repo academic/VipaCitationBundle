bd653e6442e85807581c424c23c9e90ebd910854
Behram ÇELEN <behramcelen@gmail.com>
2016-12-21 22:33:11 +0300

    move Advanced Extension to OjsCitation extension for important services import :bug: fix

--- DependencyInjection/OjsCitationExtension.php
+++ DependencyInjection/OjsCitationExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class AdvancedCitationExtension extends Extension
+class OjsCitationExtension extends Extension
 {
     /**
      * {@inheritdoc}
